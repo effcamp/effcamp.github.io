28766d30aa33eeb32645defbab26fb60d603310a
Fred Campos <fcf.camposx@gmail.com>
2018-10-27 22:34:44 -0300

    finished more styling. Definitely not perfect

--- public/js/main.js
+++ public/js/main.js
@@ -2,11 +2,23 @@
 const app = new Vue({
   el: "#app",
   data: {
-    hidden: false
+    hidden: false,
+    scroll: null,
+    scrolled: 0
   },
   methods: {
     hideHeader() {
       this.hidden = !this.hidden;
+      this.scroll = window.scrollY;
+    },
+    handleScroll(event) {
+      this.hidden = true;
     }
+  },
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.handleScroll);
   }
 });
