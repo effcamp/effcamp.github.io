36924fcdd047293ac6710d7bf3096612c67cdb97
Fred Campos <fcf.camposx@gmail.com>
2018-11-14 22:50:52 -0400

    Working modal

--- public/js/main.js
+++ public/js/main.js
@@ -4,24 +4,46 @@ const app = new Vue({
   data: {
     hidden: false,
     scroll: null,
-    copyright: 2018
+    copyright: 2018,
+    projects: projectData,
+    visible: false,
+    modal: {}
+    // projects: projects
   },
   methods: {
     hideNav() {
       document.getElementById('nav-toggle').checked = false;
     },
-    hideHeader() {
-      this.hidden = !this.hidden;
-      this.scroll = window.scrollY;
+    showModal(project) {
+      this.visible = true;
+
+      this.modal = {
+        ...project
+      };
     },
-    handleScroll(event) {
-      this.hidden = true;
+    hideModal() {
+      this.visible = false;
+      this.modal = {};
     }
   },
   created() {
-    window.addEventListener('scroll', this.handleScroll);
-  },
-  destroyed() {
-    window.removeEventListener('scroll', this.handleScroll);
+    window.addEventListener('keydown', e => {
+      if (e.key === 'Escape') {
+        this.hideModal();
+      }
+    });
   }
 });
+// created() {
+//   window.addEventListener('scroll', this.handleScroll);
+// },
+// destroyed() {
+//   window.removeEventListener('scroll', this.handleScroll);
+// }
+// hideHeader() {
+//   this.hidden = !this.hidden;
+//   this.scroll = window.scrollY;
+// },
+// handleScroll(event) {
+//   this.hidden = true;
+// }
---
+++ public/js/projects.js
@@ -0,0 +1,38 @@
+const projectData = [
+  {
+    id: 1,
+    name: 'Vue Smoothie Maker App',
+    url: 'https://smoothie.fredcampos.app',
+    github: 'https://github.com/effcamp/smoothie-maker',
+    details: ['test'],
+    image: './images/smoothie.png',
+    alt: 'Vue smoothie maker'
+  },
+  {
+    id: 2,
+    name: 'Express Chat App Using Websockets',
+    url: 'https://node-chat-app-v1.herokuapp.com/',
+    github: 'https://github.com/effcamp/node-server-chat-app',
+    details: {},
+    image: './images/express-chat.png',
+    alt: 'Express Chat App using Websockets'
+  },
+  {
+    id: 3,
+    name: 'React Expense Managing app',
+    url: 'https://expense-manager-react89.herokuapp.com/',
+    github: 'https://github.com/effcamp/react-expense-tracker',
+    details: {},
+    image: './images/expensify.png',
+    alt: 'React Expense Managing app'
+  },
+  {
+    id: 4,
+    name: 'React + Express Feedback Survey app',
+    url: 'https://emaily-server-82651.herokuapp.com/',
+    github: 'https://github.com/effcamp/react-node-emailr-app',
+    details: {},
+    image: './images/emailr.png',
+    alt: 'React + Express Feedback Survey app'
+  }
+];
